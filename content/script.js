868732791fa01969f78bf893528c0cfc7745c5b9
AMESocker <aesocher@yahoo.com>
2022-10-05 16:08:59 -0700

    Added an if else for row color

--- content/script.js
+++ content/script.js
@@ -3,5 +3,21 @@
 var today = moment();
 console.log("today", today);
 
-today.format("MMM Do, YYYY")
+$("#currentDay").text(today.format("MMM Do, YYYY"));
 
+var currentHour = moment().hour();
+console.log(currentHour)
+
+var columnHour = $(".col hour")
+console.log(columnHour)
+//compare current hour to hour in day chart and set format
+if (currentHour == columnHour) {
+    //CSS present
+        ;
+} else if (currentHour > columnHour) {
+    //CSS past
+        ;    
+} else {
+    //CSS future
+    ;
+};
